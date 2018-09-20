db14965f587701585b97e29bb598f57220c88569
sainath95 <42861765+sainath95@users.noreply.github.com>
2018-09-20 09:50:00 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -4,7 +4,7 @@ public class Hello{
 		
 		for(int i=1;i<=10;i++){
 			
-			System.out.println("Hello-Worlds ... "+i);
+			System.out.println("Hello-Worlds from GITHUB ... "+i);
 			System.out.println("sai is great");
 		
 		}
